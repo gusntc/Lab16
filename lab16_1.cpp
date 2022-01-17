5967c44cbc3d035e7a992ea144e94ca71800cd6c
gusntc <Natacha_rungbanpant@cmu.ac.th>
2022-01-17 21:06:33 +0700

    Lab16_1Edit

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,59 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+int a = 5;
+string b = "A";
+string &c = b;
+int *x = &a;
+string *y = &b;
+int **z = &x;
 
-	
-	
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << y << " ";
+	cout << z << " ";
+	cout << endl;
+	cout << &a << " ";
+	cout << &b << " ";
+	cout << &c << " ";
+	cout << &x << " ";
+	cout << &y << " ";
+	cout << &z << " ";
+	cout << endl;
+c = "F";
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << y << " ";
+	cout << z << " ";
+	cout << endl;
+*y = "W";
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << y << " ";
+	cout << z << " ";
+	cout << endl;
+*x = 6;
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << y << " ";
+	cout << z << " ";
+	cout << endl;
+**z = 7;
+	cout << a << " ";
+	cout << b << " ";
+	cout << c << " ";
+	cout << x << " ";
+	cout << y << " ";
+	cout << z << " ";
+	cout << endl;
 	return 0;
 }
